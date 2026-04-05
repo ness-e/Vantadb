1ce7d255a1ee704ab76a5c336ccc393d0fbc7d92
Eros Nessy <eros.messy@gmail.com>
2026-04-04 23:10:53 -0400

    fix(tests): align structured_api_v2 and index bitset filter with actual IQL/HNSW implementation

--- tests/structured_api_v2.rs
+++ tests/structured_api_v2.rs
@@ -23,7 +23,7 @@ async fn test_structured_api_v2_ids() {
         s2_id = *cortex.iter().find(|(_, n)| n.get_field("label").unwrap().as_str() == Some("S2")).unwrap().0;
     }
 
-    let relate_query = format!("RELATE #{} TO #{} WITH weight 0.8 LABEL \"test_rel\"", s1_id, s2_id);
+    let relate_query = format!("RELATE NODE#{} --\"test_rel\"--> NODE#{} WEIGHT 0.8", s1_id, s2_id);
     let res = executor.execute_hybrid(&relate_query).await.unwrap();
     
     if let ExecutionResult::Write { node_id, .. } = res {
@@ -33,7 +33,7 @@ async fn test_structured_api_v2_ids() {
     // Test de Insert Message ID
     // Simular un Thread
     executor.execute_hybrid("(INSERT :neuron {:type \"Thread\" :id 999})").await.unwrap();
-    let msg_query = "INSERT MESSAGE \"Hola Mundo\" AS ROLE \"user\" TO THREAD #999";
+    let msg_query = "INSERT MESSAGE USER \"Hola Mundo\" TO THREAD#999";
     let msg_res = executor.execute_hybrid(msg_query).await.unwrap();
 
     if let ExecutionResult::Write { node_id, .. } = msg_res {
